bc56edd09a4a4f88f2a637ab009b2433603c43c9
Yogitabeladiya <139565488+Yogitabeladiya@users.noreply.github.com>
2023-10-11 11:37:48 +0530

    Add files via upload

---
+++ asse.py
@@ -0,0 +1,13 @@
+from Afuncation import * 
+
+print("Admin")
+print("Manager")
+choice=input("Enter choice ")
+
+if choice=="1":
+    A.admin()
+    
+elif choice=="2":
+    M.Manager()
+else:
+    exit()
